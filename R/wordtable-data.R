8f48aa7817a52b3e8540d525ad4940d497f885ee
ryscott5 <ryscott5@uw.edu>
2017-06-12 21:36:07 +0000

    add spacy support slowly

---
+++ R/wordtable-data.R
@@ -0,0 +1,19 @@
+#' Example EPAR data
+#'
+#'Data downloaded from the epar website.
+#'
+#' @docType data
+#'
+#' @usage data(wordtable)
+#'
+#' @format An object of class \code{"tibble"}; see \code{\link[qtl]{tible:::tible}}.
+#'
+#' @keywords datasets
+#'
+#' @references sqlunet
+#' (\href{sqlunet.sourceforge.com}{Sourceforge})
+#'
+#' @source \href{sqlunet}{sqlunet}
+#'
+#' @examples data(wordtable)
+"wordtable"
